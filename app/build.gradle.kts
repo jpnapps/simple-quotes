b61f0bf7e38314f81271559d7fb34bdfa94632b7
jithishpn <apps4jp@gmail.com>
2025-12-26 03:55:08 +0530

    N/A-Updates  Signing config added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,15 +20,31 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("KEYSTORE_PATH") ?: "simple_quotes.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
+
+   /* buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }*/
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
