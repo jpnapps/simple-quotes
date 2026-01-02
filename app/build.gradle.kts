db82306051ddac49dbd34460c33a9f1c3436d323
jithishpn <apps4jp@gmail.com>
2026-01-02 20:16:36 +0530

    N/A-Updates  -Play Store Professional Pipeline updated to playstore-internal.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,9 +14,8 @@ android {
         applicationId = "com.jpdev.simplequotes"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
