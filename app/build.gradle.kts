90091921c646f2a97ca281f723595df5178b8665
jithishpn <apps4jp@gmail.com>
2026-01-02 17:37:55 +0530

    N/A-Updates  Signing config updated-storefile-PLAY_SERVICE_ACCOUNT_JSON

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.jpdev.simplequotes"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
